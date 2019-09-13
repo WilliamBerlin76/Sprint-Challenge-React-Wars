b42622d2de6c823ba0f737aee5e5c68ed15bed19
WilliamBerlin76 <william.berlin76@gmail.com>
2019-09-13 12:06:25 -0500

    everything styled

--- starwars/src/components/characterCard.js
+++ starwars/src/components/characterCard.js
@@ -2,8 +2,13 @@ import React from "react";
 import styled from "styled-components";
 
 const PersonCard = props => {
-
-
+    return(
+        <div className="character-card" key={props.id}>
+            <h2 className="char-name" key={props.name}>{props.name}</h2>
+            <p className="height" key={props.height}>Height: {props.height}</p>
+            <p className="weight" key={props.mass}>Weight: {props.mass}</p>
+        </div>
+    )
 }
 
 export default PersonCard
--- starwars/src/components/characterGrid.js
+++ starwars/src/components/characterGrid.js
@@ -4,26 +4,28 @@ import styled from "styled-components";
 import PersonCard from "./characterCard"
 
 export default function CardGrid() {
-    const [picture, setPicture] = useState([]);
     const [character, setCharacter] = useState([]);
-
   useEffect(() => {
     axios
-    .get('https://swapi.co/api/people')
+    .get(`https://swapi.co/api/people`)
     .then(response => {
-      const characterInfo = response.data.results;
-      console.log(characterInfo)
+      const people = response.data.results;
+      console.log(people);
+      setCharacter(people)
     })
     .catch(error => {
       console.log('no data for you', error)
     })
-  })
-
+  }, [])
 
 
   return (
       <div className='container'>
-
+          <div className='entry'>
+              {character.map(item =>{
+                  return <PersonCard key={item} name={item.name} height={item.height} mass={item.mass}/>
+              })}
+          </div>
       </div>
   )
 }
