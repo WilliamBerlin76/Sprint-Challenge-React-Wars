acc6d38d9aa3ed697dfa856cc9ef206a2ed4f0ce
WilliamBerlin76 <william.berlin76@gmail.com>
2019-09-13 12:43:18 -0500

    mvp complete

--- starwars/src/components/characterCard.js
+++ starwars/src/components/characterCard.js
@@ -2,13 +2,17 @@ import React from "react";
 import styled from "styled-components";
 
 const CharacterCard = styled.div`
-    background: #00FA9A;
-    width:25%;
+    background: #3CB371;
+    width:23%;
+    margin-top: 20px;
+`;
+const CharName = styled.h2`
+    background: #2E8B57;
 `;
 const PersonCard = props => {
     return(
         <CharacterCard key={props.id}>
-            <h2 className="char-name" key={props.name}>{props.name}</h2>
+            <CharName key={props.name}>{props.name}</CharName>
             <p className="height" key={props.height}>Height: {props.height}</p>
             <p className="weight" key={props.mass}>Weight: {props.mass}</p>
         </CharacterCard>
--- starwars/src/components/characterGrid.js
+++ starwars/src/components/characterGrid.js
@@ -21,18 +21,19 @@ const Container = styled.div`
     width: 75%;
     margin: 0 auto;
 `;
-// const EntryPoint = styled.div`
-//     display: flex;
-//     flex-direction: row;
-//     flex-wrap: wrap
-// `;
+const EntryPoint = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-evenly;
+`;
   return (
       <Container>
-          <div className='entry'>
+          <EntryPoint>
               {character.map(item =>{
                   return <PersonCard key={item} name={item.name} height={item.height} mass={item.mass}/>
               })}
-          </div>
+          </EntryPoint>
       </Container>
   )
 }
